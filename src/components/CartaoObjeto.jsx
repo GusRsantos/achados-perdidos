353aba047010d7311943556c6579528cdd7454fc
Gustavo Reserva <gustavo.santos102@aluno.senai.br>
2024-10-30 00:16:34 -0300

    aiai

--- src/components/CartaoObjeto.jsx
+++ src/components/CartaoObjeto.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import styles from "./CartaoObjeto.module.css";
+import React from 'react';
+import styles from './CartaoObjeto.module.css';
+
 const CartaoObjeto = ({ name, status }) => {
   return (
     <div className={styles.card}>
- <div className={styles.image}>Foto do objeto</div>
+      <div className={styles.image}>Foto do objeto</div>
       <h3>{name}</h3>
       <p className={status === 'Achado' ? styles.achado : styles.perdido}>
         {status}
@@ -13,4 +14,4 @@ const CartaoObjeto = ({ name, status }) => {
   );
 };
 
-export default CartaoObjeto
+export default CartaoObjeto;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import styles from './Home.module.css';
-import logo from "../images/logo-senai.png"; // Importe sua logo
+import logo from "../images/logo-senai.png";
+import CartaoObjeto from '../components/CartaoObjeto';
 
 function Home() {
+  const objetos = [
+    { name: 'Garrafa Stanley', status: 'Achado' },
+    { name: 'Garrafa Farm', status: 'Perdido' },
+    { name: 'Garrafa Pacco', status: 'Achado' },
+    { name: 'Garrafa Tupperware', status: 'Perdido' },
+  ];
+  console.log(objetos);
+
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
+      <header className={styles.header}>
         <img src={logo} alt="Logo Senai" className={styles.logo} />
         <div className={styles.searchContainer}>
-          <i className="bi bi-search" style={{ color: 'blue', marginRight: '8px' }}></i>
           <input type="text" placeholder="Buscar..." className={styles.searchInput} />
+          <i className="bi bi-search" style={{ color: 'blue', marginLeft: '8px' }}></i>
         </div>
-      </div>
+      </header>
 
-      {/* Adicionando a linha abaixo do cabeçalho */}
       <hr className={styles.divider} />
 
-      {/* Botão "+ OBJETO" */}
       <button className={styles.addButton}>+ OBJETO</button>
+
+      <main className={styles.mainContent}>
+        <div className={styles.legend}>
+          <span className={styles.achado}>Achado</span>
+          <span className={styles.perdido}>Perdido</span>
+        </div>
+
+        <div className={styles.cartoesContainer}>
+          {objetos.map((objeto, index) => (
+            <CartaoObjeto key={index} name={objeto.name} status={objeto.status} />
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
