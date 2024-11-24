81a644db75518a0b093591bb87b8c0b63b7ab789
Gustavo Reserva <gustavo.santos102@aluno.senai.br>
2024-11-24 05:24:50 -0300

    Acho que agora foi!

--- server/index.js
+++ server/index.js
@@ -227,7 +227,7 @@ app.get("/", (req, res) => {
 const conn = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Morningstar_20",
     database: "achadosperdidos"
 })
 
